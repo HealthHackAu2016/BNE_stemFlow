945ec5808bc1bd6d306187f95d77eac29aef5b4f
Peita Lin <n6378056@gmail.com>
2016-10-15 23:59:59 +1000

    react d3 fails

--- public_html/app.js
+++ public_html/app.js
@@ -6,28 +6,28 @@ import * as d3 from 'd3';
 
 const multiview = (divID) => {
     var options = setup_data("#" + divID);
-     init(options);
-    // var name = divID + "-wrapper";
-    // var name_html_search = "#" + name;
-   //  var scale_x = 0.3;
-   //  var scale_y = 0.3;
-   //
-   // // options.scale_x = scale_x;
-   // // options.scale_y = scale_y;
-   //
-   // var listSVG = d3.select("#" + name)
-   //         .append('svg')
-   //         .attr('id', 'listSVG');
-   //
-   // var gSVG = listSVG.append("g")
-   //         .attr("id", name)
-   //         .attr("transform", "scale(" + scale_x + "," + scale_y + ")")
-   //         // .on("contextmenu", d3.contextMenu(menu));
-   //
-   //  gSVG.append('svg').attr('id', name);
-   //  // inject SVG graph into dom elemenet
-   //  init(options);
+    //  init(options);
+    var name = divID + "-wrapper"
+    var scale_x = 0.3;
+    var scale_y = 0.3;
+
+   // options.scale_x = scale_x;
+   // options.scale_y = scale_y;
+
+   var listSVG = d3.select("#" + name).selectAll('div')
+           .append('svg')
+           .attr('id', 'listSVG');
+
+   var gSVG = listSVG.append("g")
+           .attr("id", name)
+           .attr("transform", "scale(" + scale_x + "," + scale_y + ")")
+           // .on("contextmenu", d3.contextMenu(menu));
+
+    gSVG.append('svg').attr('id', name);
+    // inject SVG graph into dom elemenet
+    init(options);
 }
+
 
 
 class Graph extends React.Component {
@@ -63,23 +63,21 @@ class App extends React.Component {
         }
         return (
             <div>
-                <h1>R: STEMFLOW</h1>
-                <div id="svgContainer">
-                    <div id="column1" style={divStyle}>
-                        <Graph id="g1"/>
-                        <Graph id="g2"/>
-                        <Graph id="g3"/>
-                    </div>
-                    <div id="column2" style={divStyle}>
-                        <Graph id="g4"/>
-                        <Graph id="g5"/>
-                        <Graph id="g6"/>
-                    </div>
-                    <div id="column3" style={divStyle}>
-                        <div id="g7"></div>
-                        <div id="g8"></div>
-                        <div id="g9"></div>
-                    </div>
+                <h1>React: STEMFLOW</h1>
+                <div id="column1" style={divStyle}>
+                    <Graph id="g1"/>
+                    <Graph id="g2"/>
+                    <Graph id="g3"/>
+                </div>
+                <div id="column2" style={divStyle}>
+                    <Graph id="g4"/>
+                    <Graph id="g5"/>
+                    <Graph id="g6"/>
+                </div>
+                <div id="column3" style={divStyle}>
+                    <div id="g7"></div>
+                    <div id="g8"></div>
+                    <div id="g9"></div>
                 </div>
             </div>
         );
